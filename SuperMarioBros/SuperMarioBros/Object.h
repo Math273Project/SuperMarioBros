f9ae7abf97c951fe38a6c206d33d8eb9166b1d95
Harry-He <hehaoqian@live.com>
2014-03-21 10:12:51 -0700

    changes object

--- SuperMarioBros/SuperMarioBros/Object.h
+++ SuperMarioBros/SuperMarioBros/Object.h
@@ -17,15 +17,21 @@ public:
 	void setPosition(int x, int y);
 	virtual bool draw(); 
 	virtual bool move(int direction, int distance);
+	bool isCollide(const Object& object);
+	bool virtual dosomething(); // Check every other Objects position or velocity and do sth according to it
+								// Change name?
 	
 protected:
 	int x_;
 	int y_;
+	int vx_;
+	int vy_;
 	int width_;
 	int height_;
 	bool isEnabled_;
+	bool isPassable_; // example: block, not passable. coins passable.
 	int priority_; // The object with higher priority will be drawn at the top of the object with low priority.
 					// Change a name?
-	char* pic_; // What is shown on the screen. The typename should not char*, but I forgot what it should be.
+	LPD3DXSPRITE sprite_; // What is shown on the screen. The typename should not char*, but I forgot what it should be.
 
 };
