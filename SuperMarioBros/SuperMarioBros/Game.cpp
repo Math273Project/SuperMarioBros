252f39700619bcaf736c8be1db2340dcbae72580
Gonzy0409 <sergon0409@gmail.com>
2014-03-21 12:21:56 -0700

    added game engine header, stable update

--- SuperMarioBros/SuperMarioBros/Game.cpp
+++ SuperMarioBros/SuperMarioBros/Game.cpp
@@ -9,3 +9,48 @@ Game::Game()
 Game::~Game()
 {
 }
+
+LRESULT Game::messageHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+
+	return 0;
+}
+
+void Game::initialize(HWND hwnd)
+{
+
+}
+
+void Game::run(HWND)
+{
+
+}
+
+void Game::releaseAll()
+{
+
+}
+
+void Game::resetAll()
+{
+
+}
+
+void Game::deleteAll()
+{
+
+}
+
+void Game::renderGame()
+{
+}
+
+void Game::update()
+{
+
+}
+
+void Game::collisions()
+{
+
+}
