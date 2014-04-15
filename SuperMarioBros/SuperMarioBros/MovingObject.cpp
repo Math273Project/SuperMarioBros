097699f2330bffe3abf19373507fd77bbf604962
Gonzy0409 <sergon0409@gmail.com>
2014-04-15 08:49:48 -0700

    merging

---
+++ SuperMarioBros/SuperMarioBros/MovingObject.cpp
@@ -0,0 +1,47 @@
+#include "MovingObject.h"
+
+void MovingObject::setFacingDirection(Direction facingDirection)
+{
+	facingDirection_ = facingDirection;
+}
+
+void MovingObject::setvx(double vx)
+{
+	if (vx_ * vx < 0)
+	{
+		if (vx < 0)
+			setFacingDirection(LEFT);
+		else
+			setFacingDirection(RIGHT);
+	}
+	vx_ = vx;
+}
+
+void MovingObject::setvy(double vy)
+{
+	vy_ = vy;
+}
+
+Direction MovingObject::getFacingDirection() const
+{
+	return facingDirection_;
+}
+
+
+bool MovingObject::moveable() const
+{
+	return true;
+}
+
+
+void MovingObject::move(int time)
+{
+	x_ += vx_ * time / 1000;
+	y_ += vy_ * time / 1000;
+}
+
+
+MovingObject::MovingObject(int id, int x, int y, int vx, int vy) : Object(id, x, y), vx_(vx), vy_(vy)
+{
+
+}
